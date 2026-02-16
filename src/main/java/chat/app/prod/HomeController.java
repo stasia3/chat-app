3c42c8ceeef0a570a5749b319b2041457c3e6cf9
stasia3 <fanaanastasia@gmail.com>
2026-02-16 15:25:12 +0200

    Minimal Spring Boot + Thymeleaf page

---
+++ src/main/java/chat/app/prod/HomeController.java
@@ -0,0 +1,16 @@
+package chat.app.prod;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "DevSocial");
+        model.addAttribute("message", "It works! Deployed on Render");
+        return "index";
+    }
+}

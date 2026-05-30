419a3594e176c2eb3bf3b526f562cf9d22cb23f5
stasia3 <fanaanastasia@gmail.com>
2026-05-30 15:25:57 +0300

    Test(post): add controller tests

---
+++ src/test/java/chat/app/prod/post/PostControllerTest.java
@@ -0,0 +1,60 @@
+package chat.app.prod.post;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(PostController.class)
+public class PostControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private PostService postService;
+
+    @Test
+    void shouldShowPostsPage() throws Exception {
+        Mockito.when(postService.getAllPosts())
+                .thenReturn(List.of());
+
+        mockMvc.perform(get("/posts"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("post/posts"))
+                .andExpect(model().attribute("title", "Posts"))
+                .andExpect(model().attributeExists("posts"));
+    }
+
+    @Test
+    void shouldShowNewPostPage() throws Exception {
+        mockMvc.perform(get("/posts/new"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("post/new-post"))
+                .andExpect(model().attribute("title", "Add Post"));
+    }
+
+    @Test
+    void shouldCreatePost() throws Exception {
+        Authentication authentication =
+                new UsernamePasswordAuthenticationToken("eva", "password");
+
+        mockMvc.perform(post("/posts/new")
+                        .principal(authentication)
+                        .param("content", "My first post"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/profile"));
+
+        Mockito.verify(postService).createPost("eva", "My first post");
+    }
+}

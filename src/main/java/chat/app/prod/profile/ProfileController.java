a7be4b4b175b034a5da0fe9d72bf9a16153cc151
stasia3 <fanaanastasia@gmail.com>
2026-06-02 21:57:39 +0300

    Feat(profile): improved profile page, displayed new info

--- src/main/java/chat/app/prod/profile/ProfileController.java
+++ src/main/java/chat/app/prod/profile/ProfileController.java
@@ -2,6 +2,7 @@ package chat.app.prod.profile;
 
 import chat.app.prod.friend.FriendService;
 import chat.app.prod.post.PostService;
+import chat.app.prod.user.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,16 +13,22 @@ public class ProfileController {
 
     private final FriendService friendService;
     private final PostService postService;
+    private final ProfileRepository profileRepository;
 
-    public ProfileController(FriendService friendService, PostService postService) {
+    public ProfileController(FriendService friendService, PostService postService, ProfileRepository profileRepository) {
         this.friendService = friendService;
         this.postService = postService;
+        this.profileRepository = profileRepository;
     }
 
     @GetMapping("/profile")
     public String profile(Model model, Authentication authentication) {
         String username = authentication.getName();
 
+        Profile profile = profileRepository.findByUserUsername(username)
+                        .orElseThrow(() -> new RuntimeException("Profile not found"));
+
+        model.addAttribute("profile", profile);
         model.addAttribute("title", "My Profile");
         model.addAttribute("username", username);
         model.addAttribute("friendCount", friendService.countFriends(username));
